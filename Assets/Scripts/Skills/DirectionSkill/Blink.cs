4bc9bd6e47069abef728124da6f4ffcd826abf62
BenJester <jichu.yang1998@gmail.com>
2019-12-13 12:42:00 -0500

    blink

---
+++ Assets/Scripts/Skills/DirectionSkill/Blink.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skills/DirectionSkill/Blink")]
+public class Blink : DirectionSkill
+{
+    public override void Do()
+    {
+        owner.transform.position = targetPos;
+    }
+}
